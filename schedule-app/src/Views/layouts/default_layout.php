df49a84c92afbb7fa6fb968fe1d2a6791143165b
Wojciech Głogowski <exitnerd@gmail.com>
2025-01-12 13:31:28 +0100

    CSS fixes

--- schedule-app/src/Views/layouts/default_layout.php
+++ schedule-app/src/Views/layouts/default_layout.php
@@ -1,7 +1,7 @@
     <!DOCTYPE html>
     <html lang="pl">
     <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Plan ZUT</title>
@@ -111,7 +111,7 @@
     </main>
 
     <footer>
-        <p data-pl="&copy; 2025 Plan Zajęć. Wszystkie prawa zastrzeżone." data-en="&copy; 2025 Schedule Plan. All rights reserved.">&copy; 2025 Plan Zajęć. Wszystkie prawa zastrzeżone.</p>
+        <p data-pl="&copy; 2025 Plan Zajęć ZUT. Wszystkie prawa zastrzeżone." data-en="&copy; 2025 ZUT Schedule. All rights reserved.">&copy; 2025 Plan Zajęć. Wszystkie prawa zastrzeżone.</p>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
